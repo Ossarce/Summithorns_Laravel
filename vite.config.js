2c77271f47bb5ae10e6352c99e060e2db5228e33
Ossarce <est.arceguzman@gmail.com>
2024-10-28 23:05:21 -0300

    Eliminado tailwind de vite-config.js

--- vite.config.js
+++ vite.config.js
@@ -1,56 +1,22 @@
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-// import path from 'path';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: [
-//                 'resources/scss/app.scss',
-//                 'resources/js/app.js',
-//             ],
-//             refresh: true,
-//         }),
-//     ],
-//     css: {
-//         preprocessorOptions: {
-//             scss: {
-//                 api: 'modern-compiler',
-//             }
-//         }
-//     },
-// });
-
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: [
-        'resources/scss/app.scss',
-        'resources/js/app.js',
-      ],
-      refresh: true,
-    }),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@use "resources/scss/base/_variables.scss" as *;',
-      }
+    plugins: [
+        laravel({
+            input: [
+                'resources/scss/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler',
+            }
+        }
     },
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    }
-  },
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, 'node_modules'),
-    },
-  },
 });
