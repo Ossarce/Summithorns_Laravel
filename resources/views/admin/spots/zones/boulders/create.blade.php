168c0486b427aa76559690da83f499b59d0af209
Ossarce <est.arceguzman@gmail.com>
2024-11-23 00:29:44 -0300

    feat(admin): Se agrega layout a las vistas y se simplifican sus formularios: - Se añade el layout de administracion a las vistas create y edit. - Se simplica el formulario, por ahora solo acepta el nombre y grado del boulder. - Más campos seran añadidos en la siguiente ronda de cambios.

--- resources/views/admin/spots/zones/boulders/create.blade.php
+++ resources/views/admin/spots/zones/boulders/create.blade.php
@@ -1,34 +1,31 @@
-@if($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+@extends('layouts.admin')
+
+@section('content')
+<main class="contenedor seccion admin-content">
+    <h1 class="section-title">Añadir Nuevo Boulder</h1>
+    <a href="{{route('boulders.index', [$spot, $zone])}}"class="go-back"><p><i class='bx bx-arrow-back'></i> Volver</p></a>
+    <form action="{{route('boulders.store', [$spot,  $zone])}}" method="POST" enctype="multipart/form-data" class="form beauty-form">
+        @csrf
+        <label for="boulder-name">Nombre del Boulder</label>
+        <input id="boulder-name" type="text" name="boulder[name]">
+
+        <label for="boulder-grade">Grado</label>
+        <select id="boulder-grade" name="boulder[grade]">
+            <option value="" selected disabled>-- Seleccione Uno --</option>
+            @foreach ($boulderGrades as $boulderGrade)
+                <option value="{{$boulderGrade->id}}">{{$boulderGrade->boulder_grade}}</option>
             @endforeach
-        </ul>
-    </div>
-@endif
-<h1>Añadir Nuevo Boulder</h1>
-<a href="{{route('boulders.index', [$spot, $zone])}}"><p>Volver</p></a>
-<form action="{{route('boulders.store', [$spot,  $zone])}}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <label for="boulder-name">Nombre del Boulder</label>
-    <input id="boulder-name" type="text" name="boulder[name]">
+        </select>
 
-    <label for="boulder-grade">Grado</label>
-    <select id="boulder-grade" name="boulder[grade]">
-        <option value="" selected disabled>-- Seleccione Uno --</option>
-        @foreach ($boulderGrades as $boulderGrade)
-            <option value="{{$boulderGrade->id}}">{{$boulderGrade->boulder_grade}}</option>
-        @endforeach
-    </select>
+        {{-- <label for="boulder-image">Imagen del Boulder <span>(opcional)</span></label>
+        <input id="boulder-image" type="file" name="boulder[image]"> --}}
 
-    <label for="boulder-image">Imagen del Boulder <span>(opcional)</span></label>
-    <input id="boulder-image" type="file" name="boulder[image]">
+        {{-- <label for="boulder-image">Abridor <span>(comunicarse con soporte de no estar en el listado de usuarios)</span></label> ***Se implementara en un futuro cercano***--}}
 
-    {{-- <label for="boulder-image">Abridor <span>(comunicarse con soporte de no estar en el listado de usuarios)</span></label> ***Se implementara en un futuro cercano***--}}
+        {{-- <label for="boulder-details">Detalles del Boulder <span>(opcional)</span></label>
+        <textarea id="boulder-details" name="boulder[details]" cols="30" rows="10"></textarea> --}}
 
-    <label for="boulder-details">Detalles del Boulder <span>(opcional)</span></label>
-    <textarea id="boulder-details" name="boulder[details]" cols="30" rows="10"></textarea>
-
-    <button type="submit">Añadir Boulder</button>
-</form>
+        <button type="submit" class="button blue-button">Añadir Boulder</button>
+    </form>
+</main>
+@endsection
--- resources/views/admin/spots/zones/boulders/edit.blade.php
+++ resources/views/admin/spots/zones/boulders/edit.blade.php
@@ -1,36 +1,33 @@
-@if($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+@extends('layouts.admin')
+
+@section('content')
+<main class="contenedor seccion admin-content">
+    <h1 class="section-title">Editar Boulder: "{{$boulder->name}}"</h1>
+    <a href="{{route('boulders.index', [$spot, $zone])}}" class="go-back"><p><i class='bx bx-arrow-back'></i> Volver</p></a>
+    <form action="{{route('boulders.update', [$spot, $zone, $boulder])}}" method="POST" class="form beauty-form">
+        @csrf
+        @method('PUT')
+        <label for="boulder-name">Nombre del Boulder</label>
+        <input id="boulder-name" type="text" name="boulder[name]" value="{{$boulder->name}}">
+
+        <label for="boulder-grade">Grado</label>
+        <select id="boulder-grade" name="boulder[grade]">
+            <option value="" disabled>-- Selecione Uno --</option>
+            @foreach ($boulderGrades as $boulderGrade)
+                <option value="{{$boulderGrade->id}}" {{$boulderGrade->id === $boulder->grade_id ? 'selected' : ''}}>{{$boulderGrade->boulder_grade}}</option>
             @endforeach
-        </ul>
-    </div>
-@endif
-<h1>Editar Boulder: "{{$boulder->name}}"</h1>
-<a href="{{route('boulders.index', [$spot, $zone])}}"><p>Volver</p></a>
-<form action="{{route('boulders.update', [$spot, $zone, $boulder])}}" method="POST">
-    @csrf
-    @method('PUT')
-    <label for="boulder-name">Nombre del Boulder</label>
-    <input id="boulder-name" type="text" name="boulder[name]" value="{{$boulder->name}}">
+        </select>
 
-    <label for="boulder-grade">Grado</label>
-    <select id="boulder-grade" name="boulder[grade]">
-        <option value="" disabled>-- Selecione Uno --</option>
-        @foreach ($boulderGrades as $boulderGrade)
-            <option value="{{$boulderGrade->id}}" {{$boulderGrade->id === $boulder->grade_id ? 'selected' : ''}}>{{$boulderGrade->boulder_grade}}</option>
-        @endforeach
-    </select>
+        {{-- <label for="boulder-image">Imagen del Boulder <span>(opcional)</span></label>
+        <input id="boulder-image" type="file" name="boulder[image]">
+        @if ($boulder->image)
+            <img src="{{asset('storage/images/spots/zones/boulders' . $boulder->image)}}" alt="Imagen Boulder {{$boulder->name}}">
+        @endif
 
-    <label for="boulder-image">Imagen del Boulder <span>(opcional)</span></label>
-    <input id="boulder-image" type="file" name="boulder[image]">
-    @if ($boulder->image)
-        <img src="{{asset('storage/images/spots/zones/boulders' . $boulder->image)}}" alt="Imagen Boulder {{$boulder->name}}">
-    @endif
+        <label for="boulder-details">Detalles del Boulder <span>(opcional)</span></label>
+        <textarea id="boulder-details" name="boulder[details]" cols="30" rows="10">{{$boulder->details}}</textarea> --}}
 
-    <label for="boulder-details">Detalles del Boulder <span>(opcional)</span></label>
-    <textarea id="boulder-details" name="boulder[details]" cols="30" rows="10">{{$boulder->details}}</textarea>
-
-    <button type="submit">Editar Boulder</button>
-</form>
+        <button type="submit" class="button blue-button">Editar Boulder</button>
+    </form>
+</main>
+@endsection
