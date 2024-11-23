7f9ebedcf6baea91e6893fffbaad5d885a1db145
Ossarce <est.arceguzman@gmail.com>
2024-11-22 23:18:29 -0300

    feat(admin): Se crea el DataTable para administrar las vías de escalada y poder implementarla en vista index: - Se crea AdminClimbingRoutesDataTable para gestionar de manera dinámica las vías. - Se implementa la DataTable en la vista index para poder ver y administrar las rutas. - Se aplica el layout de administración a la vista index.

---
+++ app/DataTables/AdminClimbingRoutesDataTable.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\ClimbingRoute;
+use App\Models\ClimbingType;
+use App\Models\Zone;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class AdminClimbingRoutesDataTable extends DataTable
+{
+    protected $spotId;
+    protected $zoneId;
+
+    public function __construct($spotId = null, $zoneId = null)
+    {
+        $this->spotId = $spotId;
+        $this->zoneId = $zoneId;
+    }
+    /**
+     * Build the DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+            ->addColumn('action', function(ClimbingRoute $climbingRoute) {
+                return '<div class="datatable-actions">
+                            <a href="'. route('routes.edit', ['spot' => $this->spotId, 'zone' => $this->zoneId, 'climbingRoute' => $climbingRoute->id]) .'" class="edit-btn">
+                                <p>
+                                    <i class="bx bxs-edit bx-border"></i>
+                                </p>
+                            </a>
+                            <a href="'. route('routes.destroy', ['spot' => $this->spotId, 'zone' => $this->zoneId, 'climbingRoute' => $climbingRoute->id]) .'" class="delete-item">
+                                <p>
+                                    <i class="bx bx-trash bx-border"></i>
+                                </p>
+                            </a>
+                        </div>';
+            })
+            ->setRowId('id');
+    }
+
+    /**
+     * Get the query source of dataTable.
+     */
+    public function query(ClimbingRoute $model): QueryBuilder
+    {
+        return $model->newQuery()
+        ->where('zone_id', $this->zoneId)
+        ->with(['grade', 'setter'])
+        ->leftJoin('route_grades', 'climbing_routes.grade_id', '=', 'route_grades.id')
+        ->select('climbing_routes.*', 'route_grades.route_grade as grade_name');
+    }
+
+    /**
+     * Optional method if you want to use the html builder.
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+                    ->setTableId('climbing_routes-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    //->dom('Bfrtip')
+                    ->orderBy(0)
+                    ->selectStyleSingle()
+                    ->buttons([
+                        Button::make('excel'),
+                        Button::make('csv'),
+                        Button::make('pdf'),
+                        Button::make('print'),
+                        Button::make('reset'),
+                        Button::make('reload')
+                    ])
+                    ->language([
+                        'processing' => 'Procesando...',
+                        'search' => 'Buscar:',
+                        'lengthMenu' => 'Mostrar _MENU_',
+                        'info' => '',
+                        'infoEmpty' => '',
+                        'infoFiltered' => '(filtrado de un total de _MAX_ registros)',
+                        'loadingRecords' => 'Cargando...',
+                        'zeroRecords' => 'No se encontraron resultados',
+                        'emptyTable' => 'Ningún dato disponible en esta tabla',
+                        'paginate' => [
+                            'first' => '<<',
+                            'previous' => '<',
+                            'next' => '>',
+                            'last' => '>>'
+                        ],
+                        'aria' => [
+                            'sortAscending' => ': Activar para ordenar la columna de manera ascendente',
+                            'sortDescending' => ': Activar para ordenar la columna de manera descendente'
+                        ]
+                    ]);
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     */
+    public function getColumns(): array
+    {
+        return [
+            Column::make('name')->title('Nombre'),
+            Column::make('grade_name')->title('Grado')->orderSequence(['asc', 'desc']),
+            Column::computed('action')
+            ->exportable(false)
+            ->printable(false)
+            ->width(60)
+            ->addClass('text-center')
+            ->title(''),
+        ];
+    }
+
+    /**
+     * Get the filename for export.
+     */
+    protected function filename(): string
+    {
+        return 'AdminClimbingRoutes_' . date('YmdHis');
+    }
+}
--- resources/views/admin/spots/zones/climbing_routes/index.blade.php
+++ resources/views/admin/spots/zones/climbing_routes/index.blade.php
@@ -1,7 +1,13 @@
-<h1>Administrar Vías de {{$spot->name}} en sector {{$zone->name}}</h1>
-<a href="{{route('zones.index', [$spot])}}"><p>Volver</p></a>
-@foreach ($climbingRoutes as $climbingRoute)
-    <h2>Nombre de la Vía</h2>
-    <a href="{{route('routes.edit', [$spot, $zone, $climbingRoute])}}"><p>{{$climbingRoute->name}}</p></a>
-@endforeach
-<a href="{{route('routes.create', [$spot, $zone])}}"><button>Añadir Vía</button></a>
+@extends('layouts.admin')
+
+@section('content')
+<main class="contenedor seccion admin-content">
+    <h1 class="section-title">Administrar Vías de {{$spot->name}} en sector {{$zone->name}}</h1>
+    <a href="{{route('zones.index', [$spot])}}" class="go-back"><p><i class='bx bx-arrow-back'></i> Volver</p></a>
+    <a href="{{route('routes.create', [$spot, $zone])}}" class="yellow-button">Añadir Vía</a>
+    <div class="table-container">
+        {{ $dataTable->table() }}
+    </div>
+</main>
+{{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+@endsection
