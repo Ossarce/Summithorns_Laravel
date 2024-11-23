49ff1976725636eae252eafb601213aba16c885e
Ossarce <est.arceguzman@gmail.com>
2024-11-22 23:22:42 -0300

    feat(admin): Se agrega layout a las vistas y se simplifican sus formularios: - Se añade el layout de administracion a las vistas create y edit. - Se simplica el formulario, por ahora solo acepta el nombre y grado de la vía. - Más campos seran añadidos en la siguiente ronda de cambios.

--- resources/views/admin/spots/zones/climbing_routes/create.blade.php
+++ resources/views/admin/spots/zones/climbing_routes/create.blade.php
@@ -1,25 +1,31 @@
-<h1>Añadir Nueva Vía</h1>
-<a href="{{route('routes.index', [$spot, $zone])}}">Volver</a>
-<form action="{{route('routes.store', [$spot, $zone])}}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <label for="route-name">Nombre de la Vía</label>
-    <input id="route-name" type="text" name="route[name]">
+@extends('layouts.admin')
 
-    <label for="route-grade">Grado</label>
-    <select id="route-grade" name="route[grade]">
-        <option value="" selected disabled>-- Seleccione Uno --</option>
-        @foreach ($routeGrades as $routeGrade)
-            <option value="{{$routeGrade->id}}">{{$routeGrade->route_grade}}</option>
-        @endforeach
-    </select>
+@section('content')
+<main class="contenedor seccion admin-content">
+    <h1 class="section-title">Añadir Nueva Vía</h1>
+    <a href="{{route('routes.index', [$spot, $zone])}}" class="go-back"><p><i class='bx bx-arrow-back'></i> Volver</p></a>
+    <form action="{{route('routes.store', [$spot, $zone])}}" method="POST" enctype="multipart/form-data" class="form beauty-form">
+        @csrf
+        <label for="route-name">Nombre de la Vía</label>
+        <input id="route-name" type="text" name="route[name]">
 
-    <label for="route-image">Imagen de la Vía <span>(opcional)</span></label>
-    <input id="route-image" type="file" name="route[image]">
+        <label for="route-grade">Grado</label>
+        <select id="route-grade" name="route[grade]">
+            <option value="" selected disabled>-- Seleccione Uno --</option>
+            @foreach ($routeGrades as $routeGrade)
+                <option value="{{$routeGrade->id}}">{{$routeGrade->route_grade}}</option>
+            @endforeach
+        </select>
 
-    {{-- <label for="boulder-image">Abridor <span>(comunicarse con soporte de no estar en el listado de usuarios)</span></label> ***Se implementara en un futuro cercano***--}}
+        {{-- <label for="route-image">Imagen de la Vía <span>(opcional)</span></label>
+        <input id="route-image" type="file" name="route[image]"> --}}
 
-    <label for="route-details">Detalles de la Vía <span>(opcional)</span></label>
-    <textarea id="route-details" name="route[details]" cols="30" rows="10"></textarea>
+        {{-- <label for="boulder-image">Abridor <span>(comunicarse con soporte de no estar en el listado de usuarios)</span></label> ***Se implementara en un futuro cercano***--}}
 
-    <button type="submit">Añadir Vía</button>
-</form>
+        {{-- <label for="route-details">Detalles de la Vía <span>(opcional)</span></label>
+        <textarea id="route-details" name="route[details]" cols="30" rows="10"></textarea> --}}
+
+        <button type="submit" class="button blue-button">Añadir Vía</button>
+    </form>
+</main>
+@endsection
--- resources/views/admin/spots/zones/climbing_routes/edit.blade.php
+++ resources/views/admin/spots/zones/climbing_routes/edit.blade.php
@@ -1,27 +1,33 @@
-<h1>Editar Vía: "{{$climbingRoute->name}}"</h1>
-<a href="{{route('routes.index', [$spot, $zone])}}"><p>Volver</p></a>
-<form action="{{route('routes.update', [$spot, $zone, $climbingRoute])}}" method="POST">
-    @csrf
-    @method('PUT')
-    <label for="route-name">Nombre del route</label>
-    <input id="route-name" type="text" name="route[name]" value="{{$climbingRoute->name}}">
+@extends('layouts.admin')
 
-    <label for="route-grade">Grado</label>
-    <select id="route-grade" name="route[grade]">
-        <option value="" disabled>-- Selecione Uno --</option>
-        @foreach ($routeGrades as $routeGrade)
-            <option value="{{$routeGrade->id}}" {{$routeGrade->id === $climbingRoute->grade_id ? 'selected' : ''}}>{{$routeGrade->route_grade}}</option>
-        @endforeach
-    </select>
+@section('content')
+<main class="contenedor seccion admin-content">
+    <h1 class="section-title">Editar Vía: "{{$climbingRoute->name}}"</h1>
+    <a href="{{route('routes.index', [$spot, $zone])}}" class="go-back"><p><i class='bx bx-arrow-back'></i> Volver</p></a>
+    <form action="{{route('routes.update', [$spot, $zone, $climbingRoute])}}" method="POST" class="form beauty-form">
+        @csrf
+        @method('PUT')
+        <label for="route-name">Nombre del route</label>
+        <input id="route-name" type="text" name="route[name]" value="{{$climbingRoute->name}}">
 
-    <label for="route-image">Imagen de la Vía <span>(opcional)</span></label>
-    <input id="route-image" type="file" name="route[image]">
-    @if ($climbingRoute->image)
-        <img src="{{asset('storage/images/spots/zones/routes' . $climbingRoute->image)}}" alt="Imagen vía {{$climbingRoute->name}}">
-    @endif
+        <label for="route-grade">Grado</label>
+        <select id="route-grade" name="route[grade]">
+            <option value="" disabled>-- Selecione Uno --</option>
+            @foreach ($routeGrades as $routeGrade)
+                <option value="{{$routeGrade->id}}" {{$routeGrade->id === $climbingRoute->grade_id ? 'selected' : ''}}>{{$routeGrade->route_grade}}</option>
+            @endforeach
+        </select>
 
-    <label for="route-details">Detalles de la Vía <span>(opcional)</span></label>
-    <textarea id="route-details" name="route[details]" cols="30" rows="10">{{$climbingRoute->details}}</textarea>
+        {{-- <label for="route-image">Imagen de la Vía <span>(opcional)</span></label>
+        <input id="route-image" type="file" name="route[image]">
+        @if ($climbingRoute->image)
+            <img src="{{asset('storage/images/spots/zones/routes' . $climbingRoute->image)}}" alt="Imagen vía {{$climbingRoute->name}}">
+        @endif
 
-    <button type="submit">Editar Ruta</button>
-</form>
+        <label for="route-details">Detalles de la Vía <span>(opcional)</span></label>
+        <textarea id="route-details" name="route[details]" cols="30" rows="10">{{$climbingRoute->details}}</textarea> --}}
+
+        <button type="submit" class="button blue-button">Editar Ruta</button>
+    </form>
+</main>
+@endsection
