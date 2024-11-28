37b1b1814f88027f110a236e96d357c8a53250dd
Ossarce <est.arceguzman@gmail.com>
2024-11-27 23:49:58 -0300

    refactor(model): Se cambia en el fillable user_id por id.

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -11,7 +11,7 @@ class Profile extends Model
     use HasFactory;
 
     protected $fillable = [
-        'user_id',
+        'id',
         'first_name',
         'last_name',
         'avatar',
@@ -48,6 +48,6 @@ class Profile extends Model
     // *** Relationships ***
 
     public function user() {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'id');
     }
 }
